abf7ca35a2f9aa0a5cfa96a8ad6f65167a143604
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-13 22:15:00 +0530

    inital comit

---
+++ src/hook/custom-useState-hook.js
@@ -0,0 +1,9 @@
+function useState(initialState) {
+    let state = initialState;
+    function setState(newState) {
+      //we can also add few conditions to validate the data.
+      state = newState;
+      render(); //your custom method to trigger page refresh on state change
+    }
+    return [state, setState];
+  }
